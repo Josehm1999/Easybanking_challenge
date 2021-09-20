f218d8cdfe1c6c4cb8973dc894c78aee1c6f73a5
Jose1999 <josehm1999@gmail.com>
2021-09-20 01:33:18 -0500

    navigation bar and animated hamburger menu added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }else {
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+        header.classList.add('open');
+    }
+})
